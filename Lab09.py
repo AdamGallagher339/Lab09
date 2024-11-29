81f89e70f781dd063d4b361e9054b5b207d32df5
Adam <G00413950@atu.ie>
2024-11-29 09:49:56 +0000

    create grayscale image

--- Lab09.py
+++ Lab09.py
@@ -2,7 +2,14 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-imgOrig = cv2.imread("ATU.jpg",)
+nrows = 2
+ncols = 1
 
+imgOrig = cv2.imread("ATU.jpg",)
 imgGray = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('Atu Origional',imgOrig)
+cv2.waitKey(0)
+cv2.imshow('Grayscale',imgGray)
+cv2.waitKey(0)
 
