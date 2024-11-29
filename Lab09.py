ac858f339da0678448079655f265a5e348cfdcea
Adam <G00413950@atu.ie>
2024-11-29 10:10:37 +0000

    Create and display cornerHarris image

--- Lab09.py
+++ Lab09.py
@@ -5,24 +5,26 @@ from matplotlib import pyplot as plt
 nrows = 2
 ncols = 1
 
-imgOrig = cv2.imread("ATU.jpg",)
+imgOrig = cv2.imread("ATU1.jpg",)
 imgGray = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
-imgHarris = cv2.imread("ATU.jpg",)
+imgHarris = imgOrig.copy()
 
 cv2.imshow('Atu Origional',imgOrig)
 cv2.waitKey(0)
 cv2.imshow('Grayscale',imgGray)
 cv2.waitKey(0)
 
-B = 10
-G = 10
-R = 10
+B = 0
+G = 0
+R = 255
 blockSize = 2
 aperture_size = 3
 k = 0.03
 
+dst = cv2.cornerHarris(imgGray, blockSize, aperture_size, k)
 
-dst = cv2.cornerHarris(imgHarris, blockSize, aperture_size, k)
+qualityLevel = 0.1
+minDistance = 10
 
 threshold = 0.2; #number between 0 and 1
 for i in range(len(dst)):
@@ -30,3 +32,5 @@ for i in range(len(dst)):
         if dst[i][j] > (threshold*dst.max()):
             cv2.circle(imgHarris,(j,i),3,(B, G, R),-1)
 
+cv2.imshow('Image Harris',imgHarris)
+cv2.waitKey(0)
